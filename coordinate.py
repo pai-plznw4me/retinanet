074273db822f0606fdfb00aed9c5962d38a10b70
kimseongjung <plznw4me@publicai.co.kr>
2023-09-20 10:28:31 +0900

    retinanet from keras tutorial

---
+++ coordinate.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+def swap_xy(boxes):
+    """Swaps order the of x and y coordinates of the boxes.
+
+    Arguments:
+      boxes: A tensor with shape `(num_boxes, 4)` representing bounding boxes.
+
+    Returns:
+      swapped boxes with shape same as that of boxes.
+    """
+    return np.stack([boxes[:, 1], boxes[:, 0], boxes[:, 3], boxes[:, 2]], axis=-1)
+
+
+def convert_to_xywh(boxes):
+    """Changes the box format to center, width and height.
+
+    Arguments:
+      boxes: A tensor of rank 2 or higher with a shape of `(..., num_boxes, 4)`
+        representing bounding boxes where each box is of the format
+        `[xmin, ymin, xmax, ymax]`.
+
+    Returns:
+      converted boxes with shape same as that of boxes.
+    """
+    return np.concat(
+        [(boxes[..., :2] + boxes[..., 2:]) / 2.0, boxes[..., 2:] - boxes[..., :2]],
+        axis=-1,
+    )
+
+
+def convert_to_corners(boxes):
+    """Changes the box format to corner coordinates
+
+    Arguments:
+      boxes: A tensor of rank 2 or higher with a shape of `(..., num_boxes, 4)`
+        representing bounding boxes where each box is of the format
+        `[x, y, width, height]`.
+
+    Returns:
+      converted boxes with shape same as that of boxes.
+    """
+    return np.concatenate(
+        [boxes[..., :2] - boxes[..., 2:] / 2.0, boxes[..., :2] + boxes[..., 2:] / 2.0],
+        axis=-1,
+    )
---
+++ main_data_processing.py
@@ -0,0 +1,5 @@
+"""
+tensorflow 을 활용해 retinanet 에 입력될 data 을 전처리합니다.
+"""
+
+import tensorflow as tf
